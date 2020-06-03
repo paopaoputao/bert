86a427900e32925d553823d3301623cca204e9c0
Ren Yulong <renyulong@trinityearth.com.cn>
2020-06-03 16:53:55 +0800

    labeled text tsv reader

--- run_classifier.py
+++ run_classifier.py
@@ -779,6 +779,37 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
     features.append(feature)
   return features
 
+class LabelProcessor(DataProcessor):
+    """Processor for the News data set (GLUE version)."""
+    def __init__(self):
+        self.labels = []
+        with open('labeled_text/labels.txt') as in_file:
+           for i in in_file:
+               self.labels.append(i.strip())
+    def get_train_examples(self, data_dir):
+        self.data_dir = data_dir
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+    def get_dev_examples(self, data_dir):
+        self.data_dir = data_dir
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+    def get_test_examples(self, data_dir):
+        self.data_dir = data_dir
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+    def get_labels(self):
+        return self.labels
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = "%s-%s" % (set_type, i)
+            text_a = tokenization.convert_to_unicode(line[1])
+            label = tokenization.convert_to_unicode(line[0])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
 
 def main(_):
   tf.logging.set_verbosity(tf.logging.INFO)
@@ -788,6 +819,7 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
+      'label': LabelProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
